c5960ecd7b94ab0a24a7c14b83dd61a07545eeec
olitreadwell <oliver.treadwell@gmail.com>
2023-09-24 23:10:30 -0700

    feat: Added routes to fetch random product(s)
    The code changes include addition of a new import and changes in the route decorator for existing functions. The route decorators are modified to allow trailing slashes in the URL. Further, two new functions have been added: `get_random_product()` and `get_random_products(num_of_products)`. These functions support fetching either a single random product or a specified number of random products from the database respectively. The products are presented in a table format inside the response body. The `get_random_products` function also includes sorting the fetched products by ID.

--- server/routes/product_routes.py
+++ server/routes/product_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, make_response
 from models import Product
+from sqlalchemy.sql.expression import func
 
 product_routes = Blueprint("product_routes", __name__)
 
@@ -19,7 +20,7 @@ def product_string(product):
     return product_string
 
 
-@product_routes.route("/products")
+@product_routes.route("/products/", strict_slashes=False)
 def get_all_products():
     products = Product.query.all()
 
@@ -46,7 +47,7 @@ def get_all_products():
     return response
 
 
-@product_routes.route("/products/<int:id>")
+@product_routes.route("/products/<int:id>/", strict_slashes=False)
 def get_product_by_id(id):
     product = Product.query.filter(Product.id == id).first()
 
@@ -71,5 +72,59 @@ def get_product_by_id(id):
         response_body += "</table>"
 
         response = make_response(response_body, 200)
+
+    return response
+
+
+@product_routes.route("/products/random/", strict_slashes=False)
+def get_random_product():
+    product = Product.query.order_by(func.random()).first()
+
+    response_body = f"""<h1>Random Product</h1>
+                        <table>
+                            <tr>
+                                <th>id</th>
+                                <th>name</th>
+                                <th>description</th>
+                                <th>price</th>
+                                <th>quantity</th>
+                                <th>category</th>
+                                <th>image_url</th>
+                            </tr>
+                            {product_string(product)}
+                        </table>
+                        """
+
+    response = make_response(response_body, 200)
+
+    return response
+
+
+@product_routes.route("/products/random/<int:num_of_products>/", strict_slashes=False)
+def get_random_products(num_of_products):
+    products = Product.query.order_by(func.random()).limit(num_of_products)
+
+    # sort products by id
+    products = sorted(products, key=lambda product: product.id)
+
+    response_body = """<h1>Random Products</h1>
+                        <table>
+                            <tr>
+                                <th>id</th>
+                                <th>name</th>
+                                <th>description</th>
+                                <th>price</th>
+                                <th>quantity</th>
+                                <th>category</th>
+                                <th>image_url</th>
+                            </tr>
+                        """
+
+    for product in products:
+        response_body += product_string(product)
+
+    response_body += "</table>"
+
+    response = make_response(response_body, 200)
 
     return response
