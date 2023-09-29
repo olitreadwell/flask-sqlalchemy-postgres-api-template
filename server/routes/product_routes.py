cf6744055c6f2d51f0427a5d9f218171facf925c
olitreadwell <oliver.treadwell@gmail.com>
2023-09-29 01:38:26 -0700

    Refactor product API endpoints
    The product endpoints in the server have been updated to match a "/api/v1" root route. This ensures consistency in API versioning. HTML response bodies for product details have been replaced with JSON results and sorted. This includes endpoints to fetch all products, a specific product by an ID, a random product, and a specific number of random products. Error handling has been improved for scenarios where the product doesn't exist. The changes offer better organization, enhanced readability, and more maintainable code.

--- server/routes/product_routes.py
+++ server/routes/product_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, make_response
+from flask import Blueprint, jsonify, make_response
 from models import Product
 from sqlalchemy.sql.expression import func
 
@@ -20,87 +20,90 @@ def product_string(product):
     return product_string
 
 
-@product_routes.route("/products/", strict_slashes=False)
+api_v1 = "/api/v1"
+
+
+@product_routes.route(api_v1 + "/products/", strict_slashes=False)
 def get_all_products():
-    products = Product.query.all()
+    products = []
 
-    response_body = """<h1>Products</h1>
-                    <table>
-                        <tr>
-                            <th>id</th>
-                            <th>name</th>
-                            <th>description</th>
-                            <th>price</th>
-                            <th>quantity</th>
-                            <th>category</th>
-                            <th>image_url</th>
-                        </tr>
-                    """
+    all_products = Product.query.all()
+    all_products = sorted(all_products, key=lambda product: product.id)
 
-    for product in products:
-        response_body += product_string(product)
+    for product in all_products:
+        product_dict = {
+            "id": product.id,
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "quantity": product.quantity,
+            "category": product.category,
+            "image_url": product.image_url,
+        }
+        products.append(product_dict)
 
-    response_body += "</table>"
-
-    response = make_response(response_body, 200)
+    response = make_response(
+        jsonify(products), 200, {"Content-Type": "application/json"}
+    )
 
     return response
 
 
-@product_routes.route("/products/<int:id>/", strict_slashes=False)
+@product_routes.route(api_v1 + "/products/<int:id>/", strict_slashes=False)
 def get_product_by_id(id):
     product = Product.query.filter(Product.id == id).first()
 
     if not product:
-        response = make_response("<h1>404 product not found</h1>", 404)
+        response = make_response(
+            jsonify({"error": f"Product with id {id} not found"}),
+            404,
+            {"Content-Type": "application/json"},
+        )
     else:
-        response_body = """<h1>Products</h1>
-                    <table>
-                        <tr>
-                            <th>id</th>
-                            <th>name</th>
-                            <th>description</th>
-                            <th>price</th>
-                            <th>quantity</th>
-                            <th>category</th>
-                            <th>image_url</th>
-                        </tr>
-                    """
+        product = {
+            "id": product.id,
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "quantity": product.quantity,
+            "category": product.category,
+            "image_url": product.image_url,
+        }
 
-        response_body += product_string(product)
-
-        response_body += "</table>"
-
-        response = make_response(response_body, 200)
+        response = make_response(
+            jsonify(product), 200, {"Content-Type": "application/json"}
+        )
 
     return response
 
 
-@product_routes.route("/products/random/", strict_slashes=False)
+@product_routes.route(api_v1 + "/products/random/", strict_slashes=False)
 def get_random_product():
     product = Product.query.order_by(func.random()).first()
 
-    response_body = f"""<h1>Random Product</h1>
-                        <table>
-                            <tr>
-                                <th>id</th>
-                                <th>name</th>
-                                <th>description</th>
-                                <th>price</th>
-                                <th>quantity</th>
-                                <th>category</th>
-                                <th>image_url</th>
-                            </tr>
-                            {product_string(product)}
-                        </table>
-                        """
+    if not product:
+        response_body = {"error": "No products found"}
+    else:
+        response_body = {
+            "id": product.id,
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "quantity": product.quantity,
+            "category": product.category,
+            "image_url": product.image_url,
+        }
 
-    response = make_response(response_body, 200)
+    response = make_response(
+        jsonify(response_body), 200, {"Content-Type": "application/json"}
+    )
 
     return response
 
 
-@product_routes.route("/products/random/<int:num_of_products>/", strict_slashes=False)
+@product_routes.route(
+    api_v1 + "/products/random/<int:num_of_products>/", strict_slashes=False
+)
 def get_random_products(num_of_products):
     products = Product.query.order_by(func.random()).limit(num_of_products)
 
