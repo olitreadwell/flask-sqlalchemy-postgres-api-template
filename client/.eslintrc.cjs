20d0dbcc02e19922ef8d2045f1b447256b5e4006
olitreadwell <oliver.treadwell@gmail.com>
2023-10-01 23:09:17 -0700

    Updated ESLint config with new set of rules
    The ESLint configuration has undergone significant changes. The new rules enforce stylistic conventions, best practices, and JavaScript language features, serving to guide the code towards improved readability, maintainability, and reduction of potential bugs. Covering aspects from indentation, line breaks, quote usage, to limiting line & function lengths and controlling code complexity, these rules help enforce consistency and aid in following recommended practices in JavaScript and React programming.

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -12,6 +12,101 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
+    // Stylistic rules
+    indent: ["warn", 4, { SwitchCase: 1 }],
+
+    // Enforces consistent linebreak style (unix)
+    "linebreak-style": ["warn", "unix"],
+
+    // Enforces the use of single quotes for strings
+    quotes: ["warn", "double"],
+
+    // Enforces semicolons at the end of statements
+    semi: ["warn", "always"],
+
+    // Limits lines of code and comments to 60 characters per line
+    "max-len": ["warn", { code: 80, comments: 80 }],
+
+    // Best practices
+    // Warns against the use of console statements
+    "no-console": "warn",
+
+    // JavaScript language features
+
+    // Warns about unreachable code
+    "no-unreachable": "warn",
+
+    // Warns about unused expressions
+    "no-unused-expressions": "warn",
+
+    // Warns about unused labels
+    "no-unused-labels": "warn",
+
+    // Warns about unused variables
+    "no-unused-vars": "warn",
+
+    // Warns about empty blocks/statements
+    "no-empty": "warn",
+
+    // Warns about empty character classes in regular expressions
+    "no-empty-character-class": "warn",
+
+    // Warns about empty function declarations
+    "no-empty-function": "warn",
+
+    // Warns about empty object patterns
+    "no-empty-pattern": "warn",
+
+    // Warns about unnecessary boolean casts
+    "no-extra-boolean-cast": "warn",
+
+    // Warns about unnecessary semicolons
+    "no-extra-semi": "warn",
+
+    // Warns about unnecessary lone blocks
+    "no-lone-blocks": "warn",
+
+    // Warns about unnecessary function callings
+    "no-useless-call": "warn",
+
+    // Warns about unnecessary try/catch blocks
+    "no-useless-catch": "warn",
+
+    // Warns about unnecessary string concatenation
+    "no-useless-concat": "warn",
+
+    // Warns about unnecessary constructor declarations
+    "no-useless-constructor": "warn",
+
+    // Warns about unnecessary escape characters
+    "no-useless-escape": "warn",
+
+    // Warns about unnecessary return statements
+    "no-useless-return": "warn",
+
+    // Warns about code complexity exceeding a maximum threshold
+    complexity: ["warn", { max: 15 }],
+
+    // Warns about excessive nested code blocks
+    "max-depth": "warn",
+
+    // Warns about files exceeding a maximum number of lines (excluding blank lines and comments)
+    "max-lines": [
+      "warn",
+      { max: 60, skipBlankLines: true, skipComments: true },
+    ],
+
+    // Warns about functions exceeding a maximum number of lines (excluding blank lines and comments)
+    "max-lines-per-function": [
+      "warn",
+      { max: 40, skipBlankLines: true, skipComments: true },
+    ],
+
+    // Warns about excessive nested callbacks
+    "max-nested-callbacks": "warn",
+
+    // Warns about functions exceeding a maximum number of statements
+    "max-statements": "warn",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
