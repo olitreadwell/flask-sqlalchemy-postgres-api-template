a9f533c16911f205e6423b196d79f3e2b7572f9c
olitreadwell <oliver.treadwell@gmail.com>
2023-10-01 23:13:14 -0700

    feat: Transformed user_routes to return JSON instead of HTML
    Overall aim of the changes made in this commit is to transform the server's user routes to return JSON instead of HTML. This includes the paths for fetching all users, a user by their id, a random user, and a selected number of random users. This will make the server's API more useful for client applications. Any error messages are also updated to be returned as JSON. The routes have also been updated to utilize a versioned path for the API.

--- server/routes/user_routes.py
+++ server/routes/user_routes.py
@@ -1,179 +1,102 @@
-from flask import Blueprint, make_response
+from flask import Blueprint, jsonify, make_response
 from models import User
 from sqlalchemy.sql.expression import func
 
 user_routes = Blueprint("user_routes", __name__)
 
 
-def user_string(user):
-    user_string = f"""
-                        <tr>
-                            <td>{user.id}</td>
-                            <td>{user.username}</td>
-                            <td>{user.email}</td>
-                            <td>{user.first_name}</td>
-                            <td>{user.last_name}</td>
-                        </tr>
-                    """
-    return user_string
+api_v1 = "/api/v1"
 
 
-@user_routes.route("/users/", strict_slashes=False)
+@user_routes.route(api_v1 + "/users/", strict_slashes=False)
 def get_all_users():
-    users = User.query.all()
+    users = []
 
-    response_body = """<h1>Users</h1>
-                    <table>
-                        <tr>
-                            <th>id</th>
-                            <th>username</th>
-                            <th>email</th>
-                            <th>first_name</th>
-                            <th>last_name</th>
-                        </tr>
-                    """
+    all_users = User.query.all()
+    all_users = sorted(all_users, key=lambda user: user.id)
 
-    for user in users:
-        response_body += user_string(user)
+    for user in all_users:
+        user_dict = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        }
+        users.append(user_dict)
 
-    response_body += "</table>"
-
-    response = make_response(response_body, 200)
+    response = make_response(jsonify(users), 200, {"Content-Type": "application/json"})
 
     return response
 
 
-@user_routes.route("/users/<int:id>")
+@user_routes.route(api_v1 + "/users/<int:id>/", strict_slashes=False)
 def get_user_by_id(id):
     user = User.query.filter(User.id == id).first()
 
-    user_products = user.user_products
-    user_products = sorted(
-        user_products, key=lambda user_product: user_product.product.category
-    )
-    user_products_string = """<h1>User Products</h1>
-                            <table>
-                                <tr>
-                                    <th>product category</th>
-                                    <th>product_id</th>
-                                    <th>product name</th>
-                                    <th>price</th>
-                                    <th>quantity</th>
-                                </tr>
-                            """
-
-    for user_product in user_products:
-        user_products_string += f"""
-                                <tr>
-                                    <td>{user_product.product.category}</td>
-                                    <td>{user_product.product.id}</td>
-                                    <td>{user_product.product.name}</td>
-                                    <td>{user_product.product.price}</td>
-                                    <td>{user_product.product.quantity}</td>
-                                </tr>
-                                """
-
-    user_products_string += "</table>"
-
     if not user:
-        response = make_response("<h1>404 user not found</h1>", 404)
+        response = make_response(
+            jsonify({"error": f"User with id {id} not found"}),
+            404,
+            {"Content-Type": "application/json"},
+        )
     else:
-        response_body = f"""<h1>User</h1>
-                            <table>
-                                <tr>
-                                    <th>id</th>
-                                    <th>username</th>
-                                    <th>email</th>
-                                    <th>first_name</th>
-                                    <th>last_name</th>
-                                </tr>
-                                {user_string(user)}
-                            </table>
-                            {user_products_string}
-                            """
+        user = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        }
 
-        response = make_response(response_body, 200)
+        response = make_response(
+            jsonify(user), 200, {"Content-Type": "application/json"}
+        )
 
     return response
 
 
-@user_routes.route("/users/random/", strict_slashes=False)
+@user_routes.route(api_v1 + "/users/random/", strict_slashes=False)
 def get_random_user():
     user = User.query.order_by(func.random()).first()
 
-    user_products = user.user_products
-    user_products = sorted(
-        user_products, key=lambda user_product: user_product.product.category
-    )
-    user_products_string = """<h1>User Products</h1>
-                            <table>
-                                <tr>
-                                    <th>product category</th>
-                                    <th>product_id</th>
-                                    <th>product name</th>
-                                    <th>price</th>
-                                    <th>quantity</th>
-                                </tr>
-                            """
-
-    for user_product in user_products:
-        user_products_string += f"""
-                                <tr>
-                                    <td>{user_product.product.category}</td>
-                                    <td>{user_product.product.id}</td>
-                                    <td>{user_product.product.name}</td>
-                                    <td>{user_product.product.price}</td>
-                                    <td>{user_product.product.quantity}</td>
-                                </tr>
-                                """
-
-    user_products_string += "</table>"
-
     if not user:
-        response = make_response("<h1>404 user not found</h1>", 404)
+        response_body = {"error": "No users found"}
     else:
-        response_body = f"""<h1>Random User</h1>
-                            <table>
-                                <tr>
-                                    <th>id</th>
-                                    <th>username</th>
-                                    <th>email</th>
-                                    <th>first_name</th>
-                                    <th>last_name</th>
-                                </tr>
-                                {user_string(user)}
-                            </table>
-                            {user_products_string}
-                            """
+        response_body = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        }
 
-        response = make_response(response_body, 200)
+    response = make_response(
+        jsonify(response_body), 200, {"Content-Type": "application/json"}
+    )
 
     return response
 
 
-@user_routes.route("/users/random/<int:num_of_users>/", strict_slashes=False)
+@user_routes.route(api_v1 + "/users/random/<int:num_of_users>/", strict_slashes=False)
 def get_random_users(num_of_users):
-    users = User.query.order_by(func.random()).limit(num_of_users)
+    returned_users = []
 
-    # sort users by id
-    users = sorted(users, key=lambda user: user.id)
+    random_users = User.query.order_by(func.random()).limit(num_of_users)
+    random_users = sorted(random_users, key=lambda user: user.id)
 
-    response_body = """<h1>Random Users</h1>
-                        <table>
-                            <tr>
-                                <th>id</th>
-                                <th>username</th>
-                                <th>email</th>
-                                <th>first_name</th>
-                                <th>last_name</th>
-                            </tr>
-                        """
+    for user in random_users:
+        user_dict = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        }
+        returned_users.append(user_dict)
 
-    for user in users:
-        response_body += user_string(user)
-
-    response_body += "</table>"
-
-    response = make_response(response_body, 200)
+    response = make_response(
+        jsonify(returned_users), 200, {"Content-Type": "application/json"}
+    )
 
     return response
