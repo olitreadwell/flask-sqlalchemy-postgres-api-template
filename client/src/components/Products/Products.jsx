a1d94db7ba100a593da81b519794a85e3bf7009b
olitreadwell <oliver.treadwell@gmail.com>
2023-10-07 23:34:25 -0700

    Updated Products and Users components
    The Products and Users components have been updated. Previously, they were just static sections displaying the titles "Products" and "Users" respectively. Now, they import useState and useEffect from React, and utilize them to fetch data from a local API, and display this dynamic content within unordered lists. Both components also keep track of their fetched data with state.

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,7 +1,36 @@
-const Products = () => (
-  <section>
-    <h1>Products</h1>
-  </section>
-);
+import { useState, useEffect } from "react";
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const url = "http://localhost:5555/api/v1/products";
+      const response = await fetch(url);
+      let json = await response.json();
+      let products = json;
+      setProducts(products);
+    };
+
+    getProducts();
+  }, []);
+
+  return (
+    <section>
+      <h1>Products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={`${product.id}-${product.name}`}>
+            <p>{product.id}</p>
+            <p>{product.name}</p>
+            <p>{product.category}</p>
+            <p>{product.price}</p>
+            <p>{product.quantity}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default Products;
--- client/src/components/Users/Users.jsx
+++ client/src/components/Users/Users.jsx
@@ -1,7 +1,36 @@
-const Users = () => (
-  <section>
-    <h1>Users</h1>
-  </section>
-);
+import { useState, useEffect } from "react";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const url = "http://localhost:5555/api/v1/users";
+      const response = await fetch(url);
+      let json = await response.json();
+      let users = json;
+      setUsers(users);
+    };
+
+    getUsers();
+  }, []);
+
+  return (
+    <section>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={`${user.id}-${user.username}`}>
+            <p>{user.id}</p>
+            <p>{user.username}</p>
+            <p>{user.first_name}</p>
+            <p>{user.last_name}</p>
+            <p>{user.email}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default Users;
